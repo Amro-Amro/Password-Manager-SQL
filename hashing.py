490880e8f3f9dbdbddc5a3aa370095eff0514fa9
Amro Abu-atieh <160271190+Amro-Amro@users.noreply.github.com>
2025-03-09 16:21:58 -0500

    Update hashing.py

--- hashing.py
+++ hashing.py
@@ -1,4 +1,18 @@
 from hashlib import sha256
 import random
+import string
 from secret import get_secret_key
 
+secret = get_secret_key()
+
+def make_password(text, name):
+  salt = get_hexdigest(secret, name)[:20]
+  hash = get_hexdigest(salt, text)
+  return ''.join(salt, hsh)
+
+def get_hexdigest(salt, text):
+  return sha256(salt + text).hexdigest()
+
+def password(text, name, length):
+  raw_hex = make_password(text, name)
+  Alphabet = 
